8054ee85225ca2be00021cc828917d173ce43147
Akhsaul <ikhsanmaulana384@gmail.com>
2022-04-25 02:09:10 +0700

    split activity
    split calculatorActivity and mainActivity

--- app/src/main/java/ikhsan/maulana/tugas/MainActivity.java
+++ app/src/main/java/ikhsan/maulana/tugas/MainActivity.java
@@ -1,84 +1,22 @@
 package ikhsan.maulana.tugas;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.view.View;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import ikhsan.maulana.tugas.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding bind;
-    private static final char[] empty = new char[1];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        bind = ActivityMainBinding.inflate(getLayoutInflater());
-        bind.btnAdd.setOnClickListener(this::add);
-        bind.btnMin.setOnClickListener(this::min);
-        bind.btnMulti.setOnClickListener(this::multi);
-        bind.btnDivide.setOnClickListener(this::div);
-        bind.btnReset.setOnClickListener(this::reset);
+        var bind = ActivityMainBinding.inflate(getLayoutInflater());
+        bind.btnCalc.setOnClickListener(v -> {
+            var destination = new Intent(this, CalculatorActivity.class);
+            startActivity(destination);
+        });
         setContentView(bind.getRoot());
-    }
-
-    private void add(View v) {
-        Integer bil1 = getVal(bind.inBil1.getText());
-        Integer bil2 = getVal(bind.inBil2.getText());
-        if (bil1 != null && bil2 != null) {
-            setVal(bil1 + bil2);
-        }
-    }
-
-    @Nullable
-    private Integer getVal(@NonNull Editable txt) {
-        String val = txt.toString();
-        if (!val.isEmpty() && !val.equals(" ")) {
-            try {
-                return Integer.parseInt(val);
-            } catch (Throwable t) {
-                return null;
-            }
-        } else {
-            return null;
-        }
-    }
-
-    private void setVal(int result) {
-        bind.txtHasil.setText(String.valueOf(result));
-    }
-
-    private void min(View v) {
-        Integer bil1 = getVal(bind.inBil1.getText());
-        Integer bil2 = getVal(bind.inBil2.getText());
-        if (bil1 != null && bil2 != null) {
-            setVal(bil1 - bil2);
-        }
-    }
-
-    private void multi(View v) {
-        Integer bil1 = getVal(bind.inBil1.getText());
-        Integer bil2 = getVal(bind.inBil2.getText());
-        if (bil1 != null && bil2 != null) {
-            setVal(bil1 * bil2);
-        }
-    }
-
-    private void div(View v) {
-        Integer bil1 = getVal(bind.inBil1.getText());
-        Integer bil2 = getVal(bind.inBil2.getText());
-        if (bil1 != null && bil2 != null) {
-            setVal(bil1 / bil2);
-        }
-    }
-
-    private void reset(View v) {
-        bind.inBil1.setText(empty, 0, empty.length);
-        bind.inBil2.setText(empty, 0, empty.length);
-        bind.txtHasil.setText(R.string.hsl);
     }
 }
