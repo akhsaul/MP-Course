fdfd0ac5e44e178dfc1237fd60833b2ce6b569da
Akhsaul <ikhsanmaulana384@gmail.com>
2022-04-25 02:14:11 +0700

    Add Data Activity

---
+++ app/src/main/java/ikhsan/maulana/tugas/AddDataActivity.java
@@ -0,0 +1,18 @@
+package ikhsan.maulana.tugas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ikhsan.maulana.tugas.databinding.ActivityAddDataBinding;
+
+public class AddDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        var bind = ActivityAddDataBinding.inflate(getLayoutInflater());
+        bind.btnBack.setOnClickListener(v -> finish());
+        setContentView(bind.getRoot());
+    }
+}
