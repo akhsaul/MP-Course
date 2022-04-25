46cc6b6848da745fa7db728a6424bacf9f9d1593
Akhsaul <ikhsanmaulana384@gmail.com>
2022-04-25 14:09:34 +0700

    add function for view details data

--- app/src/main/java/ikhsan/maulana/tugas/DetailsDataActivity.java
+++ app/src/main/java/ikhsan/maulana/tugas/DetailsDataActivity.java
@@ -12,6 +12,22 @@ public final class DetailsDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         var bind = ActivityDetailsDataBinding.inflate(getLayoutInflater());
+        var helper = new DBHelper(this);
+        try(var db = helper.getReadableDatabase();
+            var cursor = db.rawQuery(DBHelper.SELECT +
+                            " where no = " +
+                            (char) 39 + getIntent().getExtras().getString("no") + (char) 39,
+                    null)) {
+            if (cursor.getCount() > 0){
+                cursor.moveToFirst();
+                bind.inNumber.setText(cursor.getString(0));
+                bind.inName.setText(cursor.getString(1));
+                bind.inNoHp.setText(cursor.getString(2));
+                bind.inJK.setText(cursor.getString(3));
+                bind.inAddress.setText(cursor.getString(4));
+            }
+        }
+        bind.btnBack.setOnClickListener(v -> finish());
         setContentView(bind.getRoot());
     }
 }
