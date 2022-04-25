a06f7bb3fcf0f85c774dd4c1a49aca5727386c42
Akhsaul <ikhsanmaulana384@gmail.com>
2022-04-25 13:40:05 +0700

    Details Data Activity

---
+++ app/src/main/java/ikhsan/maulana/tugas/DetailsDataActivity.java
@@ -0,0 +1,14 @@
+package ikhsan.maulana.tugas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailsDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_data);
+    }
+}
